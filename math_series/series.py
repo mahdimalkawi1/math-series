714531e07d74d101e294a029e4582fd4d2e0f0c4
mahdimalkawi1 <mahdimalkawi66@gmail.com>
2023-05-08 04:51:46 +0000

    docstring

--- math_series/series.py
+++ math_series/series.py
@@ -1,24 +1,40 @@
 def fibonacci(n):
-    if n == 1 or n == 0:
+     '''
+        this function calculate the fibonacci for anumber 
+         :param n: int
+         :return: int
+    '''
+
+     if n == 1 or n == 0:
             return n
-    else:
+     else:
           return fibonacci(n-1) + fibonacci(n-2)
 
 def lucas(n):
-    if n == 0:
+     '''
+        this function calculate the lucas for anumber 
+         :param n: int
+         :return: int
+    '''
+     if n == 0:
           return 2
-    elif n == 1:
+     elif n == 1:
           return 1
-    else:
+     else:
           return lucas(n - 1) + lucas(n - 2)
     
 def sum_series(n,x=0,y=1):
+     '''
+        this function calculate the sum_series for anumber 
+         :params n,x,y: int
+         :return: int
+    '''
   
-   if x == 0 and y == 1:
+     if x == 0 and y == 1:
         return fibonacci(n)
-   elif x == 2 and y == 1:
+     elif x == 2 and y == 1:
         return lucas(n)
-   else:
+     else:
         if n == 0:
           return x
         elif n == 1:
